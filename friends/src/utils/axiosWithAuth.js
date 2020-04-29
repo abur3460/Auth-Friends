837ffd17be0139c6541f6de791e75a52a21d42de
Austin Burton <abur3460@gmail.com>
2020-04-28 19:33:33 -0600

    changes

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosWithAuth = () => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return axios.create({
     baseURL: "http://localhost:5000/api",
     headers: {
       "Content-Type": "application/json",
-      Authorization: token
-    }
+      Authorization: token,
+    },
   });
 };
 
